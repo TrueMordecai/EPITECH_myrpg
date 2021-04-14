901b15d0caa932a49a73f20f352a343105d09038
Yanis Guaye <yanis.guaye@epitech.eu>
2021-04-14 05:45:07 +0200

    libmy: update to release 0.3.5
    (cherry picked from commit b6ebf5e1e5f2c7da9136996dcf1b795f0837216a)
    Signed-off-by: Yanis Guaye <yanis.guaye@epitech.eu>

--- lib/my/src/memory/my_memrev.c
+++ lib/my/src/memory/my_memrev.c
@@ -1,12 +1,11 @@
 /*
 ** EPITECH PROJECT, 2021
-** rpg
+** LibMy - memory module
 ** File description:
-** reverse memory
+** Reverses elements in the given array
 */
 
 #include <assert.h>
-#include <stdint.h>
 
 #include "libmy/memory/memory.h"
 
@@ -14,14 +13,9 @@ MY_MEMORY_API void *my_memrev(void *mem, size_t nmemb, size_t size)
 {
     size_t half_len = nmemb / 2;
 
-    assert(mem != NULL);
-    for (size_t i = 0; i < half_len; i++) {
-        void const **restrict a = ((char *)mem) + i * size;
-        void const **restrict b = ((char *)mem) + (nmemb - (i + 1)) * size;
-
-        *a = (void *)((uintptr_t)*a ^ (uintptr_t)*b);
-        *b = (void *)((uintptr_t)*a ^ (uintptr_t)*b);
-        *a = (void *)((uintptr_t)*a ^ (uintptr_t)*b);
-    }
+    assert(size == 0 || mem != NULL);
+    for (size_t i = 0; i < half_len; ++i)
+        my_memswap((char *)mem + i * size,
+            (char *)mem + (nmemb - (i + 1)) * size, size);
     return mem;
 }
