eb3276a87b7724e0617c22d46773574bcdd37793
Yanis Guaye <yanis.guaye@epitech.eu>
2021-04-27 17:25:51 +0200

    libmy: update to 0.3.8
    collections module: fix my_vec_clear to not free the vector's capacity

--- lib/my/src/collections/vec/my_vec_clear.c
+++ lib/my/src/collections/vec/my_vec_clear.c
@@ -1,8 +1,14 @@
 /*
 ** EPITECH PROJECT, 2021
+<<<<<<< HEAD
 ** rpg
 ** File description:
 ** my_vec_clear
+=======
+** LibMy - collections module
+** File description:
+** Flushes the elements of a vector
+>>>>>>> bb5f890 (libmy: update to 0.3.8)
 */
 
 #include "libmy/collections/vec.h"
@@ -10,11 +16,21 @@
 MY_COLLECTIONS_API void my_vec_clear(my_vec_t *vec, void (*elem_free)(void *))
 {
     assert(vec != NULL);
+<<<<<<< HEAD
     if (elem_free) {
         size_t len = vec->length;
     
+=======
+    if (elem_free != NULL) {
+        size_t len = vec->length;
+
+>>>>>>> bb5f890 (libmy: update to 0.3.8)
         for (size_t i = 0; i < len; ++i)
             (*elem_free)(my_vec_get(vec, i));
     }
     vec->length = 0;
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> bb5f890 (libmy: update to 0.3.8)
