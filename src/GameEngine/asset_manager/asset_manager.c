484a9de26bbdd38a05ad0d2313156206cd57ec06
Andreas Leroux <andreas.leroux@epitech.eu>
2021-05-05 19:11:13 +0200

    Set default texture, start default font

--- src/GameEngine/asset_manager/asset_manager.c
+++ src/GameEngine/asset_manager/asset_manager.c
@@ -12,6 +12,9 @@
 #include "functions.h"
 #include "GameEngine/asset_manager.h"
 
+sfTexture *get_null_texture(void);
+sfFont *get_null_font(void);
+
 static const my_map_kvtypes_t FONTS_KVTYPES = {
     .key_size = sizeof(char *),
     .key_align = alignof(char *),
@@ -70,6 +73,8 @@ sfTexture *get_texture(asset_manager_t *asset_manager, char const *name)
 {
     asset_t *asset = my_hash_map_get(&asset_manager->textures, &name);
 
+    if (asset == NULL || asset->texture == NULL)
+        return get_null_texture();
     return asset == NULL ? NULL : asset->texture;
 }
 
@@ -77,5 +82,7 @@ sfFont *get_font(asset_manager_t *asset_manager, char const *name)
 {
     asset_t *asset = my_hash_map_get(&asset_manager->fonts, &name);
 
+    if (asset == NULL || asset->font == NULL)
+        return get_null_font();
     return asset == NULL ? NULL : asset->font;
 }
--- src/GameEngine/asset_manager/load_textures.c
+++ src/GameEngine/asset_manager/load_textures.c
@@ -5,21 +5,66 @@
 ** load textures
 */
 
+#include <SFML/Graphics.h>
+#include "GameEngine/font_default.h"
 #include "GameEngine/asset_manager.h"
+
+static void init_pixels(sfUint32 *buffer)
+{
+    for (int x = 0; x < 16; x++)
+        for (int y = 0; y < 16; y++)
+            buffer[x + y * 32] = 0xff000000;
+    for (int x = 16; x < 32; x++)
+        for (int y = 0; y < 16; y++)
+            buffer[x + y * 32] = 0xffdc00ff;
+    for (int y = 16; y < 32; y++)
+        for (int x = 0; x < 16; x++)
+            buffer[x + y * 32] = 0xffdc00ff;
+    for (int x = 16; x < 32; x++)
+        for (int y = 16; y < 32; y++)
+            buffer[x + y * 32] = 0xff000000;
+}
+
+sfTexture *get_null_texture(void)
+{
+    static sfTexture *err_texture = NULL;
+    
+    if (err_texture == NULL) {
+        sfUint32 buffer[1024] = {0};
+        sfImage *img;
+
+        init_pixels(buffer);
+        img = sfImage_createFromPixels(32, 32, (sfUint8*)buffer);
+        err_texture = sfTexture_createFromImage(img, NULL);
+        sfTexture_setRepeated(err_texture, 1);
+        sfImage_destroy(img);
+    }
+    return err_texture;
+}
+
+sfFont *get_null_font(void)
+{
+    static sfFont *err_font = NULL;
+    
+    if (err_font == NULL) {
+        err_font = sfFont_createFromMemory(ERR_FONT, ERR_FONT_SIZE);
+    }
+    return err_font;
+}
 
 static void load_fonts(asset_manager_t *manager)
 {
     load_font(manager, "roboto_font", "assets/Fonts/roboto_regular.ttf");
     load_font(manager, "pixel", "assets/Fonts/pixel.ttf");
     load_font(manager, "pixel2", "assets/Fonts/pixel2.ttf");
+    get_null_font();
 }
 
 void load_all_textures(asset_manager_t *manager)
 {
     load_texture(
         manager, "items_tooltip", "assets/Inventory/items_tooltip.png", 0);
-    load_texture(
-        manager, "inv_menu", "assets/Inventory/inventory_menu.png", 0);
+    load_texture(manager, "inv_menu", "assets/Inventory/inventory_menu.png", 0);
     load_texture(manager, "menu_bg", "assets/Textures/menu_bg.jpeg", 0);
     load_texture(manager, "pause_bg", "assets/Textures/pause_bg.png", 0);
     load_texture(manager, "settings_bg", "assets/Textures/settings_bg.jpg", 0);
@@ -28,11 +73,11 @@ void load_all_textures(asset_manager_t *manager)
     load_texture(manager, "buttons", "assets/Textures/buttons.png", 1);
     load_texture(manager, "turn_pass", "assets/Textures/turn_pass.png", 0);
     load_texture(manager, "spells", "assets/Textures/spells.png", 0);
-    load_texture(
-        manager, "Knight", "assets/Textures/Characters/Knight.png", 0);
+    load_texture(manager, "Knight", "assets/Textures/Characters/Knight.png", 0);
     load_texture(
         manager, "Skeleton", "assets/Textures/Characters/skeleton.png", 0);
     load_texture(
         manager, "Green Man", "assets/Textures/Characters/Greenman.png", 0);
     load_fonts(manager);
+    get_null_texture();
 }
