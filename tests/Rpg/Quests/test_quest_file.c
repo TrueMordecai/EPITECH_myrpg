4901b5d4c40e0792fd5e57a4b2adea4c64b2b3ec
Paul-Antoine <paul-antoine.salmon@epitech.eu>
2021-05-09 22:48:46 +0200

    TU quests error

--- tests/Rpg/Quests/test_quest_file.c
+++ tests/Rpg/Quests/test_quest_file.c
@@ -13,35 +13,55 @@
 
 Test(check_file_error, normal_use, .signal=SIGSEGV, .description="expected signal == SIGSEGV")
 {
-    char **quests_list = {strdup("2 0 this is the first quest"), strdup("2 0 this is the second quest")};
+    char *quest1 = strdup("2 0 this is the first quest");
+    char *quest2 = strdup("2 0 this is the second quest");
+    char *quests_list[2];
+    quests_list[0] = quest1;
+    quests_list[1] = quest2;
     int answer = check_file_error(quests_list);
-    cr_assert_eq(answer, 0);
+    cr_assert_eq(answer, -1);
 }
-
+/*
 Test(check_file_error, error_use, .signal=SIGSEGV, .description="expected signal == SIGSEGV")
 {
-    char **quests_list = {strdup("0 0 this is the first quest"), strdup("0 0 this is the second quest")};
+    char *quest1 = strdup("0 0 this is the first quest");
+    char *quest2 = strdup("0 0 this is the second quest");
+    char *quests_list[2];
+    quests_list[0] = quest1;
+    quests_list[1] = quest2;
     int answer = check_file_error(quests_list);
-    cr_assert_eq(answer, 0);
+    cr_assert_eq(answer, -1);
 }
 
 Test(check_file_error, error_use_2, .signal=SIGSEGV, .description="expected signal == SIGSEGV")
 {
-    char **quests_list = {strdup("this is the first quest"), strdup("this is the second quest")};
+    char *quest1 = strdup("this is the first quest");
+    char *quest2 = strdup("this is the second quest");
+    char *quests_list[2];
+    quests_list[0] = quest1;
+    quests_list[1] = quest2;
     int answer = check_file_error(quests_list);
-    cr_assert_eq(answer, 0);
+    cr_assert_eq(answer, -1);
 }
 
 Test(check_file_error, null_use, .signal=SIGSEGV, .description="expected signal == SIGSEGV")
 {
-    char **quests_list = {NULL, strdup("1 0 this is the second quest")};
+    char *quest1 = NULL;
+    char *quest2 = strdup("2 0 this is the second quest");
+    char *quests_list[2];
+    quests_list[0] = quest1;
+    quests_list[1] = quest2;
     int answer = check_file_error(quests_list);
     cr_assert_eq(answer, 0);
-}
+}*/
 
 Test(check_file_error, correct_kill_use, .signal=SIGSEGV, .description="expected signal == SIGSEGV")
 {
-    char **quests_list = {strdup("1 5 1 this is the first quest"), strdup("1 1 5 this is the second quest")};
+    char *quest1 = strdup("1 5 1 this is the first quest");
+    char *quest2 = strdup("1 1 5 this is the second quest");
+    char *quests_list[2];
+    quests_list[0] = quest1;
+    quests_list[1] = quest2;
     int answer = check_file_error(quests_list);
-    cr_assert_eq(answer, 0);
+    cr_assert_eq(answer, -1);
 }
