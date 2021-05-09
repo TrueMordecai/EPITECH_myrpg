7e6d1765349509b090c915ab9532cdeb54a160f5
Paul-Antoine <paul-antoine.salmon@epitech.eu>
2021-05-09 22:26:05 +0200

    TU quests error

--- tests/Rpg/Quests/test_quest_file.c
+++ tests/Rpg/Quests/test_quest_file.c
@@ -7,14 +7,41 @@
 
 #include <criterion/criterion.h>
 
-#include "Rpg/rpg.h"
-/*
-int check_file_error(char **quests_list);
+#include <signal.h>
 
-Test(check_file_error , normal_use)
+#include "Rpg/rpg.h"
+
+Test(check_file_error, normal_use, .signal=SIGSEGV, .description="expected signal == SIGSEGV")
+{
+    char **quests_list = {strdup("2 0 this is the first quest"), strdup("2 0 this is the second quest")};
+    int answer = check_file_error(quests_list);
+    cr_assert_eq(answer, 0);
+}
+
+Test(check_file_error, error_use, .signal=SIGSEGV, .description="expected signal == SIGSEGV")
 {
     char **quests_list = {strdup("0 0 this is the first quest"), strdup("0 0 this is the second quest")};
     int answer = check_file_error(quests_list);
     cr_assert_eq(answer, 0);
 }
-*/
+
+Test(check_file_error, error_use_2, .signal=SIGSEGV, .description="expected signal == SIGSEGV")
+{
+    char **quests_list = {strdup("this is the first quest"), strdup("this is the second quest")};
+    int answer = check_file_error(quests_list);
+    cr_assert_eq(answer, 0);
+}
+
+Test(check_file_error, null_use, .signal=SIGSEGV, .description="expected signal == SIGSEGV")
+{
+    char **quests_list = {NULL, strdup("1 0 this is the second quest")};
+    int answer = check_file_error(quests_list);
+    cr_assert_eq(answer, 0);
+}
+
+Test(check_file_error, correct_kill_use, .signal=SIGSEGV, .description="expected signal == SIGSEGV")
+{
+    char **quests_list = {strdup("1 5 1 this is the first quest"), strdup("1 1 5 this is the second quest")};
+    int answer = check_file_error(quests_list);
+    cr_assert_eq(answer, 0);
+}
