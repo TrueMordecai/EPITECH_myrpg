2ea20c22032bde7a9743025bf72d7772ad81ba7f
Yanis Guaye <yanis.guaye@epitech.eu>
2021-05-09 22:51:37 +0200

    Apply the Coding Style more

--- tests/Rpg/Quests/test_quest_file.c
+++ tests/Rpg/Quests/test_quest_file.c
@@ -11,57 +11,30 @@
 
 #include "Rpg/rpg.h"
 
-Test(check_file_error, normal_use, .signal=SIGSEGV, .description="expected signal == SIGSEGV")
+Test(check_file_error, normal_use, .signal = SIGSEGV,
+    .description = "expected signal == SIGSEGV")
 {
     char *quest1 = strdup("2 0 this is the first quest");
     char *quest2 = strdup("2 0 this is the second quest");
     char *quests_list[2];
+    int answer;
+
     quests_list[0] = quest1;
     quests_list[1] = quest2;
-    int answer = check_file_error(quests_list);
-    cr_assert_eq(answer, -1);
-}
-/*
-Test(check_file_error, error_use, .signal=SIGSEGV, .description="expected signal == SIGSEGV")
-{
-    char *quest1 = strdup("0 0 this is the first quest");
-    char *quest2 = strdup("0 0 this is the second quest");
-    char *quests_list[2];
-    quests_list[0] = quest1;
-    quests_list[1] = quest2;
-    int answer = check_file_error(quests_list);
+    answer = check_file_error(quests_list);
     cr_assert_eq(answer, -1);
 }
 
-Test(check_file_error, error_use_2, .signal=SIGSEGV, .description="expected signal == SIGSEGV")
-{
-    char *quest1 = strdup("this is the first quest");
-    char *quest2 = strdup("this is the second quest");
-    char *quests_list[2];
-    quests_list[0] = quest1;
-    quests_list[1] = quest2;
-    int answer = check_file_error(quests_list);
-    cr_assert_eq(answer, -1);
-}
-
-Test(check_file_error, null_use, .signal=SIGSEGV, .description="expected signal == SIGSEGV")
-{
-    char *quest1 = NULL;
-    char *quest2 = strdup("2 0 this is the second quest");
-    char *quests_list[2];
-    quests_list[0] = quest1;
-    quests_list[1] = quest2;
-    int answer = check_file_error(quests_list);
-    cr_assert_eq(answer, 0);
-}*/
-
-Test(check_file_error, correct_kill_use, .signal=SIGSEGV, .description="expected signal == SIGSEGV")
+Test(check_file_error, correct_kill_use, .signal = SIGSEGV,
+    .description = "expected signal == SIGSEGV")
 {
     char *quest1 = strdup("1 5 1 this is the first quest");
     char *quest2 = strdup("1 1 5 this is the second quest");
     char *quests_list[2];
+    int answer;
+
     quests_list[0] = quest1;
     quests_list[1] = quest2;
-    int answer = check_file_error(quests_list);
+    answer = check_file_error(quests_list);
     cr_assert_eq(answer, -1);
 }
